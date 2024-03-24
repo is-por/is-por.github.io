63af96dd78b35bfdd0171dcebc6b2866e925ae25
eris por <is-por@hotmail.com>
2024-03-24 10:51:38 +0100

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 function button_switcher(element)
 {
-	var pressed = element.classList.contains("pressed");	
+	let pressed = element.classList.contains("pressed");	
 	pressed ? element.classList.remove("pressed") : element.classList.add("pressed");
 	pressed ? element.classList.add("not-pressed") : element.classList.remove("not-pressed");
 	return !pressed;
@@ -8,16 +8,16 @@ function button_switcher(element)
 
 function follow_button(element)
 {
-	var texts = element.getElementsByTagName("p");
+	let texts = element.getElementsByTagName("p");
 
 	texts[0].innerHTML = button_switcher(element) ? "Seguir" : "Siguiendo";
 }
 
 function engagement_button(element)
 {
-	var texts = element.getElementsByClassName("engagement_number");
+	let texts = element.getElementsByClassName("engagement_number");
 
-	var num = button_switcher(element) ? Number(texts[0].innerHTML) + 1 : Number(texts[0].innerHTML) - 1;
+	let num = button_switcher(element) ? Number(texts[0].innerHTML) + 1 : Number(texts[0].innerHTML) - 1;
 	texts[0].innerHTML = format_number(num);
 }
 
@@ -50,73 +50,78 @@ function randn_bm(min, max, skew) {
   return  Math.floor(num)
 }
 
-
-window.onload = (event) =>
+function generate_engagement(root_elm)
 {
-	//console.log("document.onload");
-	var engagement_numbers = document.getElementsByClassName("engagement_number");
+	let engagement_numbers = root_elm.getElementsByClassName("engagement_number");
 
 	for (let i = 0; i < engagement_numbers.length; i++)
 	{
-		var min = 0;
+		let min = 0;
 		if(engagement_numbers[i].classList.contains("non-zero")) min = 1;
 
 		engagement_numbers[i].innerHTML = format_number(randn_bm(min, 9999, 9));
 	}
-	
-	carga_tuits();
 }
 
 //Wikipedia
+function generate_trends()
+{
+	let base_url = "https://es.wikipedia.org/"; 
+	let url = base_url + "/w/api.php"; 
+	let params = {
+		action: "query",
+		format: "json",
+		list: "random",
+		rnlimit: document.getElementsByClassName("wiki_article").length,
+		rnnamespace : "0"
+	};
 
-var base_url = "https://es.wikipedia.org/"; 
-var url = base_url + "/w/api.php"; 
-var params = {
-    action: "query",
-    format: "json",
-    list: "random",
-    rnlimit: document.getElementsByClassName("wiki_article").length,
-	rnnamespace : "0"
-};
+	url = url + "?origin=*";
+	Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
 
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-
-fetch(url)
-    .then(function(response){return response.json();})
-    .then(function(response) 
+	fetch(url)
+	.then(function(response){return response.json();})
+	.then(function(response) 
 	{
-        var randoms = response.query.random;
-		var trend_fields = document.getElementsByClassName("wiki_article");
+		let randoms = response.query.random;
+		let trend_fields = document.getElementsByClassName("wiki_article");
 		
-        for (var r in randoms) 
+		for (let r in randoms) 
 		{
 			trend_fields[r].href = base_url+"?curid="+randoms[r].id;
-			var article = trend_fields[r].getElementsByClassName("wiki");
+			let article = trend_fields[r].getElementsByClassName("wiki");
 			article[0].innerHTML = randoms[r].title;
-        }
-    })
-    .catch(function(error){console.log(error);});
+		}
+	})
+	.catch(function(error){console.log(error);});
+}
+
 	
 //Carga txt
 
 
-function carga_tuits()
+function carga_tuits(file)
 {
-	carga_config()
-	var file = "prueba.txt";
 	fetch(file).then(function(response)
 	{
 		console.log(response)
+		console.log(response.headers.get('Last-Modified'));
 		return response.text();
 	}).then(function(data) {
-		var orig = document.getElementsByClassName("post_block")[0];
-		var plantilla = orig.cloneNode(true);
+		let orig = document.getElementsByClassName("post_block")[0];
+		let plantilla = orig.cloneNode(true);
 
-		var tweet = plantilla.getElementsByClassName("tweet_content")[0];
+		let tweet = plantilla.getElementsByClassName("tweet_content")[0];
+		
+		//replace endl with <br/> tag
+		data = data.replace(/(?:\r\n|\r|\n)/g, "<br>");
 
 		tweet.innerHTML = data;
-		var tl = document.getElementById("timeline");
+		
+		generate_engagement(plantilla);
+		
+		//insert tweet
+		let tl = document.getElementById("timeline");
 		tl.insertBefore(plantilla, tl.children[0]);
 	})
 	.catch(function(error){console.log(error);});
@@ -127,42 +132,68 @@ function carga_tuits()
 function carga_config()
 {
 	
-	var file = "config.json";
-	fetch(file).then(function(response)
+	fetch(config_file).then(function(response)
 	{
 		console.log(response)
 		return response.json();
 	}).then(function(json) {
 		console.log(json)
-		//var json = JSON.parse(data);
 		
-		//console.log(json)
+		display_name = json.display_name;
+		user_name = json.user_name;
 		
-		console.log(json.display_name)
-		console.log(json.user_name)
+		update_username(document);
 		
 		for(let i in json.tweets)
 		{
 			console.log(json.tweets[i])
+			carga_tuits(json.tweets[i])
 		}
 	})
 	.catch(function(error){console.log(error);});
 	
 	/*
-	var response = '{	"display_name":"Eris (cansada de vuestras mierdas)",	"user_name":"@ispor1",	"tweets":	[		"prueba.txt", "prueba2.txt"	]}';
+	let response = '{	"display_name":"Eris (cansada de vuestras mierdas)",	"user_name":"@ispor1",	"tweets":	[		"prueba.txt", "prueba2.txt"	]}';
 	
 	console.log(response)
-	var json = JSON.parse(response);
+	let json = JSON.parse(response);
 	
 	console.log(json)
 	
-	console.log(json.display_name)
-	console.log(json.user_name)
+	display_name = json.display_name;
+	user_name = json.user_name;
+	
+	update_username(document);
 	
 	for(let i in json.tweets)
 	{
 		console.log(json.tweets[i])
-	}
-*/
+		carga_tuits(json.tweets[i])
+	}*/
 }
 
+function update_username(root_elm)
+{
+	//do someting
+	console.log(user_name)
+	console.log(display_name)
+}
+
+
+//Global variables
+var user_name = "@erispor1";
+var display_name = "Eris (esto es una prueba)";
+var config_file = "config/config.json";
+
+//Main execution cycle
+window.onload = (event) =>
+{
+	
+	//console.log("document.onload");
+	carga_config();
+	
+	generate_trends();
+	
+	//WIP: hacer solo cuando hayan cargado los tweets
+	generate_engagement(document);
+}
