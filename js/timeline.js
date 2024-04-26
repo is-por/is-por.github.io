f6e97e8206abf2ec3200a1c1816d9db2e2e1ed6b
eris por <is-por@hotmail.com>
2024-04-26 18:16:27 +0200

    test

---
+++ js/timeline.js
@@ -0,0 +1,37 @@
+//Overwrite
+function carga_config()
+{
+	fetch(config_file).then(function(response)
+	{
+		return response.json();
+	}).then(function(json) {
+		
+		display_name = json.display_name;
+		user_name = json.user_name;
+		sheetURL = json.sheet_url+"?page=2";
+		
+		update_username(document);
+
+		carga_tuits_drive();
+	})
+	.catch(function(error){console.log(error);});
+}
+
+//Main execution cycle
+window.onload = (event) =>
+{
+	
+	//console.log("document.onload");
+	carga_config();
+	
+	generate_trends();
+	
+	//engagement del perfil/tendencias
+	generate_engagement(document);
+	
+	format_date(document);
+	
+	birthday_count();
+	
+}
+
