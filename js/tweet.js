2494c4a2403bb4337384d432ed71869cabf267e6
eris por <is-por@hotmail.com>
2024-07-19 10:42:26 +0200

    fix

--- js/tweet.js
+++ js/tweet.js
@@ -172,7 +172,7 @@ function carga_tuits(file)
 		let img = document.createElement("img");
 		img.src = image_array[i];
 		let img_link = document.createElement("a");
-		a.href = image_array[i];
+		img_link.href = image_array[i];
 		img_link.appendChild(img);
 		image_block.appendChild(img_link);
 	}
